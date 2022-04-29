017f902dfdbb67992b0c5ab893007f566d5ac983
liyang <864387496@qq.com>
2022-04-29 18:25:09 +0800

    eslint set

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'vite';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
